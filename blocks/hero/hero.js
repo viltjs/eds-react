d54d521e13a8e4995936d56a6c55688915a7dfdc
jayme.santos@vilt-group.com <jayme.santos@vilt-group.com>
2025-08-08 11:09:22 -0300

    create hero-react block

--- blocks/hero/hero.js
+++ blocks/hero/hero.js
@@ -1,7 +1,2 @@
-import { render } from '../../dist/main.js'
-
 export default function decorate(block) {
-  const component = document.createElement('div')
-  render(component, { text: 'EDS loves ReactJS!' })
-  block.appendChild(component)
 }
